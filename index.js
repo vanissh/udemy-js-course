4cb1b9d1a0e4e1aeae0a55a06f6bb47f22ed57bc
Anna <sutcliff0684@gmail.com>
2022-02-13 22:35:13 +0300

    strings

--- index.js
+++ index.js
@@ -14,4 +14,32 @@ result = Math.max(3, 2, -57, 99, 82, 13) //99
 
 result = Math.round(Math.random() * 100) // целое число от 0 до 100
 
-console.log(result)
+
+//string manipulations 
+
+const firstName = 'Jack'
+const lastName = 'Brown'
+const colors = 'red, orange, blue, green'
+const space = ' '
+ //Escaping 
+ result = "I like \"Star wars\" movie" // \ - экранирование
+
+ //methods
+result = firstName.concat(space, lastName) // 'Jack Brown'
+
+result = 'Hello'.indexOf('e') // 1
+result = 'Hello'.indexOf('lo') // 3 - индекс первого символа последовательности
+result = 'Hello'.lastIndexOf('l') // 3 - ищет символ с конца строки
+result = 'Hello'.lastIndexOf('z') // -1 - если символ отсутствует
+
+result = firstName.charAt(1) // a
+result = firstName.charAt(firstName.length - 1) // k - получаем последний символ строки
+
+result = firstName.substring(0, 3) // Jac - подстрока начинается с 0 индекса и заканчивается на 2
+
+result = firstName.slice(0, 3) // Jac - подстрока начинается с 0 индекса и заканчивается на 2
+result = firstName.slice(-3) // ack - можно извлечь последние символы в строке
+
+result = colors.replace('green', 'yellow') // 'red, orange, blue, yellow'
+
+result = colors.includes('green') // true
