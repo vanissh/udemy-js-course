8ba327072f67e8f8119df575271d3be91bb9671d
Anna <sutcliff0684@gmail.com>
2022-02-13 23:01:35 +0300

    1 task

---
+++ practice/_1.js
@@ -0,0 +1,6 @@
+let _firstName = prompt('Enter your name')
+let _lastName = prompt('Enter your last name')
+let age = prompt('Enter your age')
+
+console.log('Your name is ' + _firstName + ', you last name is ' 
+            + _lastName + '. Your age is ' + age)
